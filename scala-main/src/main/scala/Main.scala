adb4b92d05c00cc7e5523821aece91b045ee5d9b
ho-ri1991 <hage.1991@hotmail.co.jp>
2018-01-16 21:49:00 +0900

    add meta-function for generating Sequence function

--- scala-main/src/main/scala/Main.scala
+++ scala-main/src/main/scala/Main.scala
@@ -3,8 +3,15 @@ import com.hori1991.variadic._
 object Main {
   @MapGenerator(5) class Mapper
 
+  @SequenceGenerator(5) class Sequencer
+
   def main(args: Array[String]): Unit = {
     val mapper = Mapper[Option]
     println(mapper.map(Some(1), Some("a"), Some(false), Some(1.5)){ (x, y, z, w) => (x, y, z, w)})
+    println(mapper.map(Some(1), Some("a"), Some(false), None){ (x, y, z, w) => (x, y, z, w)})
+
+    val sequencer = Sequencer[Option]
+    println(sequencer.sequence(Some(1), Some("a"), Some(false), Some(1.5)))
+    println(sequencer.sequence(Some(1), None, Some(false), Some(1.5)))
   }
 }
--- scala-variadic/src/main/scala/com/hori1991/variadic/SequenceGenerator.scala
+++ scala-variadic/src/main/scala/com/hori1991/variadic/SequenceGenerator.scala
@@ -3,24 +3,24 @@ package com.hori1991.variadic
 import scala.annotation.{StaticAnnotation, compileTimeOnly}
 import scala.meta._
 
-@compileTimeOnly("MapGenerator not expanded")
-class MapGenerator(generateNum: Int) extends StaticAnnotation {
+@compileTimeOnly("SequenceGenerator not expanded")
+class SequenceGenerator(generateNum: Int) extends StaticAnnotation {
   inline def apply(defn: Any): Any = meta {
     val q"class $className" = defn
     val q"new $_(${num: Term.Arg})" = this
 
-    val mapDefs =
+    val sequenceDefs =
       for(i <- 2 to num.toString().toInt) yield {
         val genericsParams = (1 to i).map(j => s"A${j}").mkString(", ")
         val arguments = (1 to i).map(j => s"fa${j}: F[A${j}]").mkString(", ")
         val params = (1 to i).map(j => s"a${j}").mkString(", ")
-        val functionEntity = (1 to i).map(j => s"a${j} <- fa${j}").mkString("for{\n", "\n", s"\n} yield f(${params})")
-        s"def map[${genericsParams}, R](${arguments})(f: (${genericsParams}) => R): F[R] = ${functionEntity}".parse[Stat].get
+        val functionEntity = (1 to i).map(j => s"a${j} <- fa${j}").mkString("for{\n", "\n", s"\n} yield ($params)")
+        s"def sequence[${genericsParams}](${arguments}): F[(${genericsParams})] = ${functionEntity}".parse[Stat].get
       }
 
     q"""
         case class ${className}[F[X] <: {def flatMap[Y](g: X => F[Y]): F[Y]; def map[Y](g: X => Y): F[Y]}]() {
-          ..${mapDefs}
+          ..${sequenceDefs}
         }
      """
   }
